824708411ee22ebc9c0fbfa42dd1916143167a5d
Iheatu Wogu <iheatuwogu@Iheatus-MacBook-Pro.local>
2020-06-28 17:29:31 -0400

    simple tick tac table with state passed down

--- src/App.js
+++ src/App.js
@@ -1,33 +1,64 @@
-import React from 'react';
+import React, {useState, createContext} from 'react';
 import styled from 'styled-components';
 import './App.css';
 
-const TickTacToeStyle = styled.td`
+
+export const GlobalStateContext = createContext();
+
+const TickTacToeCell = ({ yIndex, xIndex }) => {
+  const displayState = oldState => ({...oldState, [yIndex]: [...oldState[yIndex], [xIndex], (oldState[yIndex][xIndex]) ? true : false ]})
+  return (
+    <GlobalStateContext.Consumer>{({ state, dispatch })  => {
+      return  <td onClick={() => console.log("dispatch ran") || dispatch(displayState)}>{state[yIndex][xIndex]}</td>}
+
+      }</GlobalStateContext.Consumer>
+  );
+}
+
+const TickTacToeStyle = styled(TickTacToeCell)`
   width: 40px;
   height: 40px;
   color: black;
   border: solid black 1px;
 `;
 
+
 function App() {
+  const [globalState, setGlobalState] = useState({
+    0: ["test", "test", "test"], 
+    1: ["test", "test", "test"],
+    2: ["test", "test", "test"],
+  });
+
+  const dispatch = (stateCreator) => setGlobalState(state => stateCreator(state));
+
+
   return (
-    <div className="App">
-      <tr>
-        <TickTacToeStyle/>
-        <TickTacToeStyle/>
-        <TickTacToeStyle/>
-      </tr>
-      <tr>
-        <TickTacToeStyle/>
-        <TickTacToeStyle/>
-        <TickTacToeStyle/>
-      </tr>
-      <tr>
-        <TickTacToeStyle/>
-        <TickTacToeStyle/>
-        <TickTacToeStyle/>
-      </tr>   
-    </div>
+    <GlobalStateContext.Provider value={{dispatch, state: globalState}}>
+      <div className="App">
+        <table>
+          <tbody>
+            <tr>
+              <TickTacToeStyle yIndex={0} xIndex={0}/>
+              <TickTacToeStyle yIndex={0} xIndex={1}/>
+              <TickTacToeStyle yIndex={0} xIndex={2}/>
+            </tr>
+            <tr>
+              <TickTacToeStyle yIndex={1} xIndex={0}/>
+              <TickTacToeStyle yIndex={1} xIndex={1}/>
+              <TickTacToeStyle yIndex={1} xIndex={2}/>
+
+            </tr>
+            <tr>
+              <TickTacToeStyle yIndex={2} xIndex={0}/>
+              <TickTacToeStyle yIndex={2} xIndex={1}/>
+              <TickTacToeStyle yIndex={2} xIndex={2}/>
+
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </GlobalStateContext.Provider>
   );
 }
 
