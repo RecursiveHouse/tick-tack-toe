10852d8ba5e560844a321309b2981eed86ae9c14
Jonathan Lorimer <jonathan_lorimer@mac.com>
2020-07-07 00:48:20 -0400

    added new fold based game mechanics

--- src/store.js
+++ src/store.js
@@ -1,10 +1,7 @@
 import { createContext } from "react";
+import { makeBoard } from "./game-mechanics";
 
-export const GAME_STATE_INIT = [
-  [" ", " ", " "],
-  [" ", " ", " "],
-  [" ", " ", " "],
-];
+export const GAME_STATE_INIT = makeBoard(3);
 
 export const GlobalStateContext = createContext({
   gameState: GAME_STATE_INIT,
